8b9738f473406ab16dfd81b2237d61747d97b866
taiyou112 <tzty140@gmail.com>
2024-09-09 16:26:01 +0900

    挙動修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  #before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  #before_action :move_to_index, except: [:index, :show]
 
 
   
@@ -46,8 +46,8 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype.destroy
-    if @prototype
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
       redirect_to root_path
     else
       redirect_to root_path
@@ -59,9 +59,4 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
